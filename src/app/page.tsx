0deb25b384701a5b153bed5e0506ae3d4a10c9e5
Douwe <douwewesterdijk@gmail.com>
2024-11-22 13:49:29 +0100

    Misc + site theme now based on sunset / sunrise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,11 @@ import Projects from "@/sections/projects";
 import AboutMe from "@/sections/aboutme";
 import Bottom from "@/sections/bottom";
 
+import Background from "@/components/background";
 import PageWrapper from "@/components/pagewrapper";
 import { isDay } from "@/util/date";
 
-import { useState, useEffect, useCallback } from "react";
-
-import { Engine } from "tsparticles-engine";
-import { loadStarsPreset } from "tsparticles-preset-stars";
-import Particles from "react-tsparticles";
+import { useState, useEffect } from "react";
 
 declare global {
 	interface Window {
@@ -21,13 +18,10 @@ declare global {
 	}
 }
 
+const day = await isDay();
+
 export default function Home() {
-	const darkTheme = "#0b090a";
-	const lightTheme = "#f8f9fa";
-
-	const [theme, setTheme] = useState(isDay() ? "light" : "dark");
-
-	const [backgroundSpeed, setBackgroundSpeedState] = useState(1);
+	const [theme, setTheme] = useState(day ? "light" : "dark");
 
 	useEffect(() => {
 		if (theme === "dark") {
@@ -37,61 +31,14 @@ export default function Home() {
 		}
 	}, [theme]);
 
-	useEffect(() => {
-		window.setBackgroundSpeed = function (speed: number): void {
-			setBackgroundSpeedState(speed);
-			console.log(`Background speed set to ${speed}`);
-		};
-	}, []);
-
 	const handleTheme = () => {
 		setTheme(theme === "light" ? "dark" : "light");
 		console.log(`Theme changed to ${theme === "light" ? "dark" : "light"}`);
 	};
 
-	const particlesInit = useCallback(async (engine: Engine) => {
-		await loadStarsPreset(engine);
-	}, []);
-
-	const particlesLoaded = useCallback(async (container: any) => {
-		await console.log(container);
-	}, []);
-
 	return (
 		<PageWrapper>
-			<Particles
-				id='tsparticles'
-				init={particlesInit}
-				loaded={particlesLoaded}
-				options={{
-					preset: "stars",
-					background: {
-						color: {
-							value: theme === "light" ? lightTheme : darkTheme,
-						},
-					},
-					particles: {
-						color: {
-							value: theme === "light" ? darkTheme : lightTheme,
-						},
-						opacity: {
-							value: 0.5,
-						},
-						move: {
-							enable: true,
-							speed: backgroundSpeed,
-						},
-						size: {
-							value: 1.5,
-						},
-						fps_limit: (backgroundSpeed * 15 < 60 ? backgroundSpeed * 15 : 60) as number,
-					},
-					fullScreen: {
-						enable: true,
-						zIndex: 0,
-					},
-				}}
-			/>
+			<Background theme={theme} />
 			<Navbar themeHandler={handleTheme} currentTheme={theme} />
 			<Name />
 			<Projects />
--- src/util/date.ts
+++ src/util/date.ts
@@ -5,15 +5,21 @@ export function getYear(): number {
 export function getAge(birthdate: string): number {
     const birth = new Date(birthdate);
     const now = new Date();
-    let age = now.getFullYear() - birth.getFullYear();
+    let age: number = now.getFullYear() - birth.getFullYear();
     if (now.getMonth() < birth.getMonth() || (now.getMonth() === birth.getMonth() && now.getDate() < birth.getDate())) {
         age--;
     }
     return age;
 }
 
-export function isDay(): boolean {
+export async function isDay() {
+    const response = await fetch('https://api.sunrisesunset.io/json?lat=53.22369&lng=6.56479');
+    const data = await response.json();
+
     const now = new Date();
-    const hour = now.getHours();
-    return hour >= 8 && hour < 21;
+
+    const sunrise = new Date(`${now.toDateString()} ${data.results.sunrise} UTC`);
+    const sunset = new Date(`${now.toDateString()} ${data.results.sunset} UTC`);
+
+    return (now >= sunrise && now <= sunset);
 }
